520b77d7316d7793258f2b5c1c290356f5995fd3
Kowal <bartlomiej319@wp.pl>
2021-02-14 19:41:14 +0100

    tests

--- src/main/java/io/github/mat3e/App.java
+++ src/main/java/io/github/mat3e/App.java
@@ -1,10 +1,39 @@
 package io.github.mat3e;
 
-/**
- * Hello world!
- */
+import org.eclipse.jetty.annotations.AnnotationConfiguration;
+import org.eclipse.jetty.plus.webapp.EnvConfiguration;
+import org.eclipse.jetty.plus.webapp.PlusConfiguration;
+import org.eclipse.jetty.webapp.WebInfConfiguration;
+import org.eclipse.jetty.webapp.WebXmlConfiguration;
+import org.eclipse.jetty.webapp.MetaInfConfiguration;
+import org.eclipse.jetty.webapp.FragmentConfiguration;
+import org.eclipse.jetty.webapp.JettyWebXmlConfiguration;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.webapp.*;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws Exception  {
+        var webapp = new WebAppContext();
+        webapp.setResourceBase("src/main/webapp");
+        webapp.setContextPath("/");
+        webapp.setConfigurations(new Configuration[]
+                {
+                        new AnnotationConfiguration(),
+                        new WebInfConfiguration(),
+                        new WebXmlConfiguration(),
+                        new MetaInfConfiguration(),
+                        new FragmentConfiguration(),
+                        new EnvConfiguration(),
+                        new PlusConfiguration(),
+                        new JettyWebXmlConfiguration()
+                });
+        webapp.setAttribute("org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", ".*/classes/.*");
+
+        var server = new Server(8080);
+    server.setHandler(webapp);
+
+    server.start();
+    server.join();
     }
 }
--- src/test/java/io/github/mat3e/HelloServiceTest.java
+++ src/test/java/io/github/mat3e/HelloServiceTest.java
@@ -1,4 +1,69 @@
 package io.github.mat3e;
 
+import org.junit.Test;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class HelloServiceTest {
+
+    private final static String WELCOME = "Hello";
+    private final static String FALLBACK_ID_WELCOME = "Hello";
+
+
+    @Test
+    public void test_prepareGreeting_nullName_returnsGreetingWithFallback() {
+        var mockRepository = alwaysReturningHelloRepository();
+        var SUT = new HelloService(mockRepository);
+        var result = SUT.prepareGreeting(null, "-1");
+        assertEquals(WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    @Test
+    public void test_prepareGreeting_name_returnsGreetingWithNameOn()  {
+        var mockRepository = alwaysReturningHelloRepository();
+        var SUT = new HelloService(mockRepository);
+        String name = "test";
+        var result = SUT.prepareGreeting(name, "-1");
+        assertEquals(WELCOME + " " + name + "!", result);
+
+    }
+    @Test
+    public void test_prepareGreeting_nullLang_returnGreetingWitFallbackIdLang() {
+        var mockRepository = fallbackLangIdRepository();
+        var SUT = new HelloService(mockRepository);
+        var result = SUT.prepareGreeting(null, null);
+        assertEquals(FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    @Test
+    public void test_prepareGreeting_textLang_returnGreetingWitFallbackIdLang() {
+        var mockRepository = fallbackLangIdRepository();
+        var SUT = new HelloService(mockRepository);
+        var result = SUT.prepareGreeting(null, "abc");
+        assertEquals(FALLBACK_ID_WELCOME + " " + HelloService.FALLBACK_NAME + "!", result);
+    }
+
+    private LangRepository fallbackLangIdRepository() {
+        return new LangRepository() {
+            @Override
+            Optional<Lang> findById(Long id) {
+                if ((id.equals(HelloService.FALLBACK_LANG.getId()))){
+                    return Optional.of((new Lang(null, FALLBACK_ID_WELCOME, null)));
+                }
+                return Optional.empty();
+            }
+        };
+    }
+
+    private LangRepository alwaysReturningHelloRepository() {
+        return new LangRepository() {
+            @Override
+            Optional<Lang> findById(Long id) {
+                return Optional.of(new Lang(null, WELCOME, "null"));
+            }
+        };
+    }
 }
